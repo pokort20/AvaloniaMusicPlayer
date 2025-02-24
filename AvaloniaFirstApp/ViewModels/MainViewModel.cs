e425ee175f0b451a098d449511398331a16ea606
pokort20 <tomaspok10@seznam.cz>
2025-02-24 20:33:02 +0100

    view model update
    view model update

--- AvaloniaFirstApp/ViewModels/MainViewModel.cs
+++ AvaloniaFirstApp/ViewModels/MainViewModel.cs
@@ -1,6 +1,31 @@
-﻿namespace AvaloniaFirstApp.ViewModels;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace AvaloniaFirstApp.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
-    public string Greeting => "Let's change things up shall we";
+    public MainViewModel()
+    {
+        AccountName = "Tomas Pokorny";
+    }
+    private string _accountName;
+    public string AccountName
+    {
+        get => _accountName;
+        set
+        {
+            if(_accountName != value)
+            {
+                _accountName = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
 }
