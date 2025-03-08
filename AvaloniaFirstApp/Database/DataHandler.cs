086f94a8e6d4d5cf06cc60764a15ffbb4a6e1b8b
pokort20 <tomaspok10@seznam.cz>
2025-03-08 22:21:56 +0100

    data handler changes
    changed data handler to create new dbcontext for each db call

--- AvaloniaFirstApp/Database/DataHandler.cs
+++ AvaloniaFirstApp/Database/DataHandler.cs
@@ -11,18 +11,16 @@ namespace AvaloniaFirstApp.Database
 {
     public class DataHandler
     {
-        private DatabaseContext db;
-        public DataHandler()
-        {
-            db = new DatabaseContext();
-        }
         public async Task<Account> GetUserAccount(int accountid)
         {
+            using var db = new DatabaseContext();
             return await db.Accounts.FirstOrDefaultAsync(account => account.id == accountid);
         }
+
         public async Task<List<Song>> TrendingSongs(int n)
         {
             Debug.WriteLine("TrendingSongs db access");
+            using var db = new DatabaseContext();
             return await db.Songs
                 .OrderBy(s => EF.Functions.Random())
                 .Take(n)
@@ -30,51 +28,63 @@ namespace AvaloniaFirstApp.Database
                 .ThenInclude(sa => sa.Artist)
                 .ToListAsync();
         }
+
         public async Task<List<Artist>> TrendingArtists(int n)
         {
-            return await db.Artists
-                .OrderBy(a => EF.Functions.Random()) // Random order
-                .Take(n)
-                .ToListAsync();
-        }
-        public async Task<List<Song>> SuggestedSongs(int n)
-        {
-            return await db.Songs
-                .OrderBy(s => EF.Functions.Random())
-                .Take(n)
-                .Include(s => s.SongArtists)
-                .ThenInclude(sa => sa.Artist)
-                .ToListAsync();
-        }
-        public async Task<List<Artist>> SuggestedArtists(int n)
-        {
+            using var db = new DatabaseContext();
             return await db.Artists
                 .OrderBy(a => EF.Functions.Random())
                 .Take(n)
                 .ToListAsync();
         }
+
+        public async Task<List<Song>> SuggestedSongs(int n)
+        {
+            using var db = new DatabaseContext();
+            return await db.Songs
+                .OrderBy(s => EF.Functions.Random())
+                .Take(n)
+                .Include(s => s.SongArtists)
+                .ThenInclude(sa => sa.Artist)
+                .ToListAsync();
+        }
+
+        public async Task<List<Artist>> SuggestedArtists(int n)
+        {
+            using var db = new DatabaseContext();
+            return await db.Artists
+                .OrderBy(a => EF.Functions.Random())
+                .Take(n)
+                .ToListAsync();
+        }
+
         public async Task<List<Song>> SearchSongs(string searchterm)
         {
+            using var db = new DatabaseContext();
             return await db.Songs
                 .Where(s => s.name.Contains(searchterm))
                 .Include(s => s.SongArtists)
                 .ThenInclude(sa => sa.Artist)
                 .ToListAsync();
         }
+
         public async Task<List<Song>> SearchSongs(Artist artist)
         {
+            using var db = new DatabaseContext();
             return await db.Songs
                 .Include(s => s.SongArtists)
                 .ThenInclude(sa => sa.Artist)
                 .Where(sa => sa.SongArtists.Any(sa => sa.Artist.id == artist.id))
                 .ToListAsync();
         }
+
         public async Task<List<Song>> SearchSongs(Playlist playlist)
         {
+            using var db = new DatabaseContext();
             return await db.Songs
                 .Include(s => s.SongPlaylists)
                     .ThenInclude(sp => sp.Playlist)
-                .Include(s => s.SongArtists) 
+                .Include(s => s.SongArtists)
                     .ThenInclude(sa => sa.Artist)
                 .Where(sp => sp.SongPlaylists.Any(sp => sp.Playlist.id == playlist.id))
                 .ToListAsync();
@@ -82,48 +92,50 @@ namespace AvaloniaFirstApp.Database
 
         public async Task<List<Album>> SearchAlbums(string searchterm)
         {
+            using var db = new DatabaseContext();
             return await db.Albums
                 .Where(a => a.name.Contains(searchterm))
                 .Include(a => a.AlbumArtists)
                 .ThenInclude(aa => aa.Artist)
                 .ToListAsync();
         }
-        //public async Task<List<Playlist>> SearchPlaylists(string searchterm)
-        //{
-        //    return await db.Playlists
-        //        .Where(p => p.name.Contains(searchterm))
-        //        .ToListAsync();
-        //}
-        
+
         public async Task<List<Podcast>> SearchPodcasts(string searchterm)
         {
+            using var db = new DatabaseContext();
             return await db.Podcasts
                 .Where(p => p.name.Contains(searchterm))
                 .Include(p => p.PodcastArtists)
                 .ThenInclude(pa => pa.Artist)
                 .ToListAsync();
         }
+
         public async Task<List<T>> SearchItems<T>(string searchTerm) where T : class
         {
+            using var db = new DatabaseContext();
             return await db.Set<T>()
                 .Where(e => EF.Property<string>(e, "name").Contains(searchTerm))
                 .ToListAsync();
         }
+
         public async Task<bool> AddToPlaylist(Song s, Playlist p)
         {
             Debug.WriteLine("Adding song " + s.name + " to playlist " + p.name);
             return false;
         }
+
         public async Task<List<Playlist>> GetFavouritePlaylists(Account account)
         {
             Debug.WriteLine("favourite playlists db access");
+            using var db = new DatabaseContext();
             return await db.Playlists
-                .Where(p => p.AccountPlaylists.Any(ap => ap.accountid == 1))
+                .Where(p => p.AccountPlaylists.Any(ap => ap.accountid == account.id))
                 .ToListAsync();
         }
 
         //public async Task<List<T>> GetAccountFavourite<T>(Account account) where T : class
         //{
+        //    using var db = new DatabaseContext();
         //    string foreignKeyName = typeof(T) switch
         //    {
         //        var t when t == typeof(Playlist) => "accountid",
@@ -139,20 +151,13 @@ namespace AvaloniaFirstApp.Database
         //        .ToListAsync();
         //}
 
-        //public async Task<List<T>> GetAccountFavourite<T>(Account account) where T : class
-        //{
-        //    return await db.Set<T>()
-        //        .Where(e => EF.Property<int>(e, "name").Contains(searchTerm))
-        //        .ToListAsync();
-        //}
         //public async Task<List<Playlist>> GetPlaylistsForAccount(Account account)
         //{
+        //    using var db = new DatabaseContext();
         //    return await db.AccountPlaylists
         //        .Where(ap => ap.accountid == account.id)
         //        .Select(ap => ap.Playlist)
         //        .ToListAsync();
         //}
-
-
     }
 }
